27a5bab55f031a6cf9c9257e5ab8029370c5b517
phil <philjord@ihug.co.nz>
2016-12-08 18:27:54 +1300

    SimpleShaderAppearance cache of attirbutesets to aggressively reuse pointers

--- src/classes/share/org/jogamp/java3d/utils/shader/SimpleShaderAppearance.java
+++ src/classes/share/org/jogamp/java3d/utils/shader/SimpleShaderAppearance.java
@@ -174,6 +174,9 @@ public class SimpleShaderAppearance extends ShaderAppearance
 	private static HashMap<GLSLShaderProgram, String> vertexShaderSources = new HashMap<GLSLShaderProgram, String>();
 	private static HashMap<GLSLShaderProgram, String> fragmentShaderSources = new HashMap<GLSLShaderProgram, String>();
 
+	private static ShaderAttributeSet baseMapShaderAttributeSet = null;
+	private static HashMap<String, ShaderAttributeSet> shaderAttributeSetCache = new HashMap<String, ShaderAttributeSet>();
+
 	private boolean buildBasedOnAttributes = false;
 
 	// we can't set it in the super class as tex coord gen is not supported in the pipeline
@@ -382,16 +385,15 @@ public class SimpleShaderAppearance extends ShaderAppearance
 					&& (this.getPolygonAttributes() == null // no poly attributes
 							|| (!this.getPolygonAttributes().isLive() && !this.getPolygonAttributes().isCompiled()) // poly attributes are not yet live
 							|| this.getPolygonAttributes().getCapability(PolygonAttributes.ALLOW_MODE_READ))//poly attributes are live but can be read
-			
-					// finally we must be allowed to set the shader program while live
-					&& ((!this.isLive() && !this.isCompiled()) || (this.getCapability(ALLOW_SHADER_PROGRAM_WRITE)))
-			)
+
+			// finally we must be allowed to set the shader program while live
+					&& ((!this.isLive() && !this.isCompiled()) || (this.getCapability(ALLOW_SHADER_PROGRAM_WRITE))))
 			{
 				boolean hasTexture = this.getTexture() != null || this.getTextureUnitCount() > 0;
 				if (this.getTextureUnitCount() > 0)
 					System.out.println("this.getTextureUnitCount() " + this.getTextureUnitCount());
 				boolean lit = this.getMaterial() != null; // having material== lit geometry
-				
+
 				//POLYGON_LINE and POLYGON_POINT are not lit
 				lit = lit && (this.getPolygonAttributes() == null
 						|| this.getPolygonAttributes().getPolygonMode() == PolygonAttributes.POLYGON_FILL);
@@ -690,22 +692,38 @@ public class SimpleShaderAppearance extends ShaderAppearance
 		vertexShaderSource = vertexShaderSources.get(shaderProgram);
 		fragmentShaderSource = fragmentShaderSources.get(shaderProgram);
 
+		//It is REALLY important for render performance to try to have the same shader program object 
+		// and the exact same shader attribute set object
 		if (hasTexture)
 		{
-			ShaderAttributeSet shaderAttributeSet = new ShaderAttributeSet();
-			shaderAttributeSet.put(new ShaderAttributeValue("BaseMap", new Integer(0)));
 			if (texCoordGenModeObjLinear)
 			{
 				Vector4f planeS = new Vector4f();
 				texCoordGeneration.getPlaneS(planeS);
 				Vector4f planeT = new Vector4f();
 				texCoordGeneration.getPlaneT(planeT);
-
-				shaderAttributeSet.put(new ShaderAttributeValue("texCoordGenPlaneS", planeS));
-				shaderAttributeSet.put(new ShaderAttributeValue("texCoordGenPlaneT", planeT));
+				String key = "planeS " + planeS + " planeT " + planeT;
+				ShaderAttributeSet shaderAttributeSet = shaderAttributeSetCache.get(key);
+				if (shaderAttributeSet == null)
+				{
+					shaderAttributeSet = new ShaderAttributeSet();
+					shaderAttributeSet.put(new ShaderAttributeValue("BaseMap", new Integer(0)));
+					shaderAttributeSet.put(new ShaderAttributeValue("texCoordGenPlaneS", planeS));
+					shaderAttributeSet.put(new ShaderAttributeValue("texCoordGenPlaneT", planeT));
+					shaderAttributeSetCache.put(key, shaderAttributeSet);
+				}
+				setShaderAttributeSet(shaderAttributeSet);
+			}
+			else
+			{
+				if (baseMapShaderAttributeSet == null)
+				{
+					baseMapShaderAttributeSet = new ShaderAttributeSet();
+					baseMapShaderAttributeSet.put(new ShaderAttributeValue("BaseMap", new Integer(0)));
+				}
+				setShaderAttributeSet(baseMapShaderAttributeSet);
 			}
 
-			setShaderAttributeSet(shaderAttributeSet);
 		}
 
 	}
