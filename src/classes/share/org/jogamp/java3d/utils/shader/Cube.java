b5dd42448b0f6ada183b9a02ff923a115e818191
phil <philjord@ihug.co.nz>
2016-12-13 16:08:33 +1300

    Cube header made to match core

--- src/classes/share/org/jogamp/java3d/utils/shader/Cube.java
+++ src/classes/share/org/jogamp/java3d/utils/shader/Cube.java
@@ -1,5 +1,30 @@
 /*
- * Created on Jan 1, 2005
+ * Copyright (c) 2016 JogAmp Community. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies,
+ * either expressed or implied, of the JogAmp Community.
+ *
  */
 package org.jogamp.java3d.utils.shader;
 
@@ -13,7 +38,9 @@ import org.jogamp.java3d.Shape3D;
 import org.jogamp.java3d.TriangleArray;
 
 /**
- * @author Administrator
+ * Note Cube does not have normals and so cannot be used for lit geometries
+ * It is designed as a general debug position indicator
+ * If you want to add a Material and have lighting enabled please use a org.jogamp.java3d.utils.geometry.Box
  *
  */
 public class Cube extends Shape3D
